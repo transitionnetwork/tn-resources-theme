34859e5e2218523cb94ae832d0b23bac1d0d69ae
Beef <mark@benewith.com>
2024-09-13 12:01:06 +0100

    update page template

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,10 +1,16 @@
-<?php if(have_rows('flexible_content')) {?>
+<main class="content">
+   
+  <h1 class=""><?php echo \Tofino\Helpers\title(); ?></h1>
 
-  <main class="text-center">
+   <?php get_template_part('templates/flexible-content/intro'); ?>
+  
+   <?php if(have_rows('flexible_content')) {?>
+
     <?php while(have_rows('flexible_content') ) : the_row();  ?>
       <?php //var_dump(get_row_layout()); ?>
       <?php get_template_part('templates/flexible-content/' . get_row_layout()); ?>
     <?php endwhile; ?>
-  </main>
 
-<?php } ?>
+  <?php } ?>
+</main>
+
