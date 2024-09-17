a2e502d0e29efbf3dcda988e4326c12d8174c5d7
Beef <mark@benewith.com>
2024-09-17 17:52:32 +0100

    tweakz

--- templates/partials/hero-banner.php
+++ templates/partials/hero-banner.php
@@ -1,6 +1,6 @@
   <div class="md:flex my-24 md:min-h-hero-screen space-y-12 md:space-y-0">
     <div class="md:w-1/3">
-      <?php echo svg(array('sprite' => 'logomark-outline-white', 'class' => 'w-full h-84 md:h-full')); ?>
+      <?php echo svg(array('sprite' => 'logomark', 'class' => 'w-full h-84 md:h-full')); ?>
     </div>
     <div class="md:w-2/3 flex items-center md:px-12 xl:px-24">
       <h1 class="space-y-6">
