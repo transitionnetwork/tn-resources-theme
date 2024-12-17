ffa148026e2600cfe760b665ae56760f3de63eaf
Beef <mark@benewith.com>
2024-12-17 13:19:58 +0000

    add acf form

--- templates/content-page-submit-resource.php
+++ templates/content-page-submit-resource.php
@@ -2,6 +2,10 @@
    
   <h1 class="h2"><?php echo \Tofino\Helpers\title(); ?></h1>
 
-  <?php acfe_form('submit-resource'); ?>
+  <?php if(is_user_logged_in()) { ?>
+    <?php acfe_form('submit-resource'); ?>
+  <?php } else { ?>
+    <?php _e('Please <a href="' . get_the_permalink(39) . '">login</a>'); ?>
+  <?php } ?>
 </main>
 
