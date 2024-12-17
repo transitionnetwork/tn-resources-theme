ab0dc9d42bf385d916841d29df388acf49276f5b
Beef <mark@benewith.com>
2024-12-17 13:38:47 +0000

    acf tweaks

--- templates/content-page-submit-resource.php
+++ templates/content-page-submit-resource.php
@@ -1,9 +1,11 @@
-<main class="content container mt-12">
+<main class="content container mt-12 space-y-12">
    
-  <h1 class="h2"><?php echo \Tofino\Helpers\title(); ?></h1>
+  <h1 class="h2 text-center"><?php echo \Tofino\Helpers\title(); ?></h1>
 
   <?php if(is_user_logged_in()) { ?>
-    <?php acfe_form('submit-resource'); ?>
+    <div class="max-w-4xl rounded-md bg-white mx-auto p-12">
+      <?php acfe_form('submit-resource'); ?>
+    </div>
   <?php } else { ?>
     <?php _e('Please <a href="' . get_the_permalink(39) . '">login</a>'); ?>
   <?php } ?>
