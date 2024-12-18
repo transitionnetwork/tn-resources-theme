8bf58e024fadc6326738aa3316b08397115756ee
Beef <mark@benewith.com>
2024-12-18 12:51:29 +0000

    add styling to acf forms

--- functions.php
+++ functions.php
@@ -37,6 +37,7 @@ $custom_includes = [
   "src/custom/register-types.php",
   "src/custom/helpers.php",
   "src/custom/routes.php",
+  "src/custom/acfe.php",
   "src/custom/ajax/write_location.php",
   "src/custom/ajax/get_popular_resources.php",
 ];
