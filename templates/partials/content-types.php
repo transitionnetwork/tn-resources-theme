d4871edadc222e4ef727a90796f35fde17ab75d9
Beef <mark@benewith.com>
2025-02-25 16:23:04 +0000

    add alt and title tags to icons

--- templates/partials/content-types.php
+++ templates/partials/content-types.php
@@ -4,7 +4,7 @@
     <div class="pr-2">
       <a class="no-underline flex items-center space-x-1 text-sm" href="<?php echo get_term_link($term); ?>">
         <?php if(get_field('term_icon', 'term_' .   $term->term_id)) { ?>
-          <img src="<?php echo get_field('term_icon', 'term_' . $term->term_id)['sizes']['thumbnail']; ?>" class="h-3 w-auto">
+          <img src="<?php echo get_field('term_icon', 'term_' . $term->term_id)['sizes']['thumbnail']; ?>" class="h-3 w-auto" alt="<?php echo get_field('term_icon', 'term_' . $term->term_id)['alt']; ?>" title="<?php echo get_field('term_icon', 'term_' . $term->term_id)['title']; ?>">
         <?php } ?>
         <span><?php echo $term->name; ?></span>
       </a>
@@ -16,7 +16,7 @@
     <div class="pr-2">
       <a class="no-underline flex items-center space-x-1 text-sm" href="<?php echo get_term_link($term); ?>">
         <?php if(get_field('term_icon', 'term_' .   $term->term_id)) { ?>
-          <img src="<?php echo get_field('term_icon', 'term_' .   $term->term_id)['sizes']['thumbnail']; ?>" class="h-3 w-auto">
+          <img src="<?php echo get_field('term_icon', 'term_' .   $term->term_id)['sizes']['thumbnail']; ?>" class="h-3 w-auto" alt="<?php echo get_field('term_icon', 'term_' . $term->term_id)['alt']; ?>" title="<?php echo get_field('term_icon', 'term_' . $term->term_id)['title']; ?>">
         <?php } ?>
         <span><?php echo $term->name; ?></span>
       </a>
@@ -29,9 +29,9 @@
       <div>
         <a class="no-underline flex items-center space-x-1 text-sm" href="<?php echo get_term_link($country); ?>">
           <?php if($country->slug !== 'global') { ?>
-            <img src="https://flagsapi.com/<?php echo strtoupper($country->slug); ?>/flat/64.png" class="h-3 w-auto">
+            <img src="https://flagsapi.com/<?php echo strtoupper($country->slug); ?>/flat/64.png" class="h-3 w-auto" alt="<?php echo $country->name; ?>" title="<?php echo $country->name; ?>">
           <?php } else { ?>
-            <img src="<?php echo get_field('term_icon', 'term_' .   $country->term_id)['sizes']['thumbnail']; ?>" class="h-3 w-auto">
+            <img src="<?php echo get_field('term_icon', 'term_' .   $country->term_id)['sizes']['thumbnail']; ?>" class="h-3 w-auto" alt="<?php echo get_field('term_icon', 'term_' . $country->term_id)['alt']; ?>" title="<?php echo get_field('term_icon', 'term_' . $country->term_id)['title']; ?>">
           <?php } ?>
           <span><?php echo $country->name; ?></span>
         </a>
