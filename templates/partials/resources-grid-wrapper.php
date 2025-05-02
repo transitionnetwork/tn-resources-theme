5174129b7b43136a7673b139da9dca0ffb3d8cb7
Beef <mark@benewith.com>
2025-05-02 12:38:02 +0100

    amend featured resrouces

--- templates/partials/resources-grid-wrapper.php
+++ templates/partials/resources-grid-wrapper.php
@@ -1,7 +1,14 @@
 <div class="bg-brand-white py-12">
   <div class="container">
     <h2 class="h3">Featured Resources</h2>
-    <?php get_template_part('templates/partials/resources-grid', null, array('posts_per_page'  => 3) ); ?>
+    <?php $resources = get_field('featured_resources', 'options'); ?>
+    <?php if($resources) { ?>
+      <div class="grid grid-cols-12 gap-6 my-12">
+        <?php foreach($resources as $post) { ?>
+          <?php get_template_part('templates/cards/resource'); ?>
+        <?php } ?>
+      </div>
+    <?php } ?>
     <div class="text-center">
       <a href="<?php echo home_url('resources'); ?>" class="text-center inline-flex space-x-2 items-center justify-center btn btn-primary">
         <span>View all resources</span>
